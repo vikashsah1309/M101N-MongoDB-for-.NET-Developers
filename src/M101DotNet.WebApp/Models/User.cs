795654e666ba5d7dad3c6c9226ad9577fd237ba3
vikashsah1309 <vkumarsah13@gmail.com>
2017-08-22 20:12:19 +0530

    MongoDbRegistrationandLogin
    MongoDbRegistrationandLogin

---
+++ src/M101DotNet.WebApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace M101DotNet.WebApp.Models
+{
+    public class User
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
